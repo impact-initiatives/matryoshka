82d39e16afb6791e2306fdf80d3e42e640dd8237
iyed.ghedamsi <iyed.ghedamsi@impact-initiatives.org>
2026-02-08 16:37:58 +0300

    feat(models): add submission-management staging model derived from audit logs

---
+++ models/staging/stg_klt__kobo_audit_log__submission_management.sql
@@ -0,0 +1,24 @@
+WITH src AS (
+    SELECT
+        *
+    FROM
+        {{ source(
+            'klt',
+            'kobo_audit_log'
+        ) }}
+)
+SELECT
+    _dlt_id AS audit_load_id,
+    TO_TIMESTAMP(
+        _dlt_load_id :: DOUBLE PRECISION
+    ) AT TIME ZONE 'UTC' AS loaded_at,
+    date_created AT TIME ZONE 'UTC' AS created_at,
+    user_uid AS user_id,
+    username AS user_name,
+    action,
+    metadata__asset_uid AS asset_id,
+    metadata__uuid AS submission_uuid
+FROM
+    src
+WHERE
+    log_type = 'submission-management'
