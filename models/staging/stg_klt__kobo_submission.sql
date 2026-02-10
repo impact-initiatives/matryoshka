23eda50b09a7e554128ae242242c0d52bbd9caef
iyed.ghedamsi <iyed.ghedamsi@impact-initiatives.org>
2026-02-10 12:07:12 +0300

    feat: add submission uuid as a key

--- models/staging/stg_klt__kobo_submission.sql
+++ models/staging/stg_klt__kobo_submission.sql
@@ -9,11 +9,12 @@ WITH src AS (
 )
 SELECT
     _id AS submission_id,
+    _uuid AS submission_uuid,
     _dlt_id AS submission_load_id,
-  asset_uid as asset_id,
-  TO_TIMESTAMP(
+    asset_uid AS asset_id,
+    TO_TIMESTAMP(
         _dlt_load_id :: DOUBLE PRECISION
     ) AT TIME ZONE 'UTC' AS loaded_at,
-  _submission_time at time zone 'utc' as submitted_at
+    _submission_time AT TIME ZONE 'utc' AS submitted_at
 FROM
     src
