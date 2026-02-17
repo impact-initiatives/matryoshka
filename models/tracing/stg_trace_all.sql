7ced11fb85e55999e3198db6b0d33d3763a0b52e
iyed.ghedamsi <iyed.ghedamsi@impact-initiatives.org>
2026-02-17 10:37:47 +0300

    feat(tracing): add dynamic staging models using dbt_utils relation discovery and unions
    Use `dbt_utils.get_relations_by_pattern` to discover DLT tracing tables (`trace*`) across `%_trace` schemas and `dbt_utils.union_relations` to combine them into unified staging models for Data Load Tool trace data.

---
+++ models/tracing/stg_trace_all.sql
@@ -0,0 +1,16 @@
+{% set table_name = 'trace' %}
+{% set relations = dbt_utils.get_relations_by_pattern(
+    schema_pattern = '%_trace',
+    table_pattern = table_name
+) %}
+{% if execute %}
+    {% if relations | length == 0 %}
+        {{ exceptions.raise_compiler_error(
+            "get_relations_by_pattern found no '" ~ table_name ~ "' tables in any schema."
+        ) }}
+    {% endif %}
+
+    {{ dbt_utils.union_relations(
+        relations = relations
+    ) }}
+{% endif %}
